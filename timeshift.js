f320161a96ba076af2a7044664e9a4ad1bf1817b
isivisi <johniannandrea@gmail.com>
2022-02-17 13:42:17 -0500

    test

---
+++ timeshift.js
@@ -0,0 +1,13 @@
+import cockpit from 'cockpit';
+
+const backupRegex = /([0-9]*).*\>.*([0-9]{4}.* ) ([A-Z].*)/;
+
+async function getBackups() {
+    cockpit.spawn(["timeshift", "--list", address.value])
+        .then(raw => {
+            console.log(raw)
+            return raw
+        });
+}
+
+getBackups()
