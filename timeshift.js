dac428baa5cb63031090c9eecf5791711297f644
isivisi <johniannandrea@gmail.com>
2022-04-25 17:35:07 -0400

    working list

--- timeshift.js
+++ timeshift.js
@@ -1,13 +1,34 @@
 //import cockpit from 'cockpit';
 
-const backupRegex = /([0-9]*).*\>.*([0-9]{4}.* ) ([A-Z].*)/;
+const backupRegex = /([0-9]*).*\>.*([0-9]{4}.* ) ([A-Z].*)/g;
+
+const backuplist = document.getElementById("backuplist");
 
 async function getBackups() {
-    cockpit.spawn(["timeshift", "--list", address.value])
-        .then(raw => {
-            console.log(raw)
-            return raw
-        });
+    var output;
+    await cockpit.spawn(["timeshift", "--list"], options={superuser:true})
+    .stream(out => {
+        console.log(output)
+        output = out.toString();
+    })
+    return output
 }
 
-await getBackups()
+async function update() {
+    getBackups().then(backupsRaw => {
+        var backups = [...backupsRaw.matchAll(backupRegex)]
+        for (var i = 0; i < backups.length; i++) {
+            var backup = backups[i]
+
+            backuplist.innerHTML += `<tr>
+                <td><img src="default.png" role="presentation" alt=""></td>
+                <td><button class="pf-c-button pf-m-link" type="button">${backup[1]}</button></td>
+                <td>${backup[2]}</td>
+                <td>${backup[3]}</td>
+                <td><button class="pf-c-button pf-m-danger" type="button">Remove</button></td>
+                </tr>`
+        }
+    })
+}
+
+update()
